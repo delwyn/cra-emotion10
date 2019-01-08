b1a47e38222f0f9504d70ec456ddc0d0d0211e0e
Delwyn de Villiers <delwyn.d@gmail.com>
2019-01-08 12:16:38 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { jsx, css } from '@emotion/core'
+import styled from '@emotion/styled'
+
+const cls = css`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <Div />
+        <header css={cls}>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
